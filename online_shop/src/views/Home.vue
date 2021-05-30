43a56a21138780346191aa63f0e7a5dd55a367b4
omar115 <omarhasan115@gmail.com>
2021-05-30 22:23:53 +0600

    fetch data using axios

--- online_shop/src/views/Home.vue
+++ online_shop/src/views/Home.vue
@@ -1,14 +1,69 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Welcome to Odileon Online Shop
+            </p>
+            <p class="subtitle">
+                The best online store available in front of you
+            </p>
+        </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+          <h2 class="is-size-2 has-text-centered"> New Arrivals</h2>
+      </div>
+
+      <ProductBox 
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+        v-bind:product="product" />
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
+// import ProductBox from '@/components/ProductBox'
 export default {
   name: 'Home',
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
   components: {
-  }
+    // ProductBox
+  },
+  mounted() {
+    this.getLatestProducts()
+    // document.title = 'Home | Djackets'
+  },
+  methods: {
+    getLatestProducts() {
+      // this.$store.commit('setIsLoading', true)
+      // await axios
+      axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      // this.$store.commit('setIsLoading', false)
+    // }
+    }
+  // }
 }
 </script>
+
+<style scoped>
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
